ca946f3f9f342b011638542c6e1c9e56d9da26ff
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2021-06-12 10:44:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client
+from pyrogram import Client, filters
 
 FayasNoushad = Client(
     "Translator Bot",
@@ -7,5 +7,25 @@ FayasNoushad = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+START_TEXT = """
+Hello {}, I am a photo to glitch art telegram bot.
+
+Made by @FayasNoushad
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+    [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+    ]]
+)
+
+@FayasNoushad.on_message(filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True
+    )
 
 FayasNoushad.run()
